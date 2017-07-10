2a59e39b8b44d10915505cc3fe1aa12c655ca551
baoqiang <baoqiang@xinhehui.com>
2017-07-10 18:21:18 +0800

    初始化

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,21 +1,21 @@
 var express = require('express')
 var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
 var config = require('../webpack.config.js')
+
 var app = express()
 var compiler = webpack(config)
-var hotMiddleware = require('webpack-hot-middleware')(compiler)
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    // 发布事件
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
-  })
-})
-app.use(hotMiddleware)
-app.listen(9999, function (err) {
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
+app.use(webpackHotMiddleware(compiler))
+app.listen(7777, function (err) {
   if (err) {
     console.log(err)
     return
   }
-  console.log('Listening at http://localhost:9999')
-})
+  console.log('Listening at http://localhost:7777')
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
+var path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
-var path = require('path');
-var webpack = require('webpack');
-// 引入基本配置
-var config = require('./webpack.config');
-var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-
-config.output.publicPath = '/';
-
-config.plugins = [
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoEmitOnErrorsPlugin(),
-  new HtmlWebpackPlugin({
-    filename: './../dists/index.html',
-    template: path.resolve(__dirname, './../demo/index.html'),
-    inject: true
-  }),
-  new FriendlyErrorsPlugin()
-];
-
-// 动态向入口配置中注入 webpack-hot-middleware/client
-var devClient = './build/dev-client';
-var extras = [devClient];
-config.entry = extras.concat(config.entry);
-
-module.exports = config;
+var webpack = require('webpack')
+module.exports = {
+  entry: ['webpack-hot-middleware/client?reload=true?http://127.0.0.1:7777/','./index.js'],
+  output: {
+    path: __dirname + '/',
+    publicPath: '/',
+    filename: 'index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './../dists/index.html',
+      template: path.resolve(__dirname, './index.html'),
+      inject: true
+    })
+  ]
+ /* module: {
+    loaders: [
+      { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude:     /node_modules/ },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      { test: /\.css$/, loader: "style!css" },
+      {test: /\.less/,loader: 'style-loader!css-loader!less-loader'}
+    ]
+  }*/
+  /*resolve:{
+    extensions:['','.js','.json']
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ]*/
+};
